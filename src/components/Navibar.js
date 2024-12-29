0076b9be9e731d7febda65d78db7e5bd48adc1cf
ASBi03 <2300016092@webmail.uad.ac.id>
2024-12-29 21:07:15 +0700

    commit

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -6,7 +6,7 @@ const Navibar = () => {
     return (
         <nav className="font-poppins border-b border-yellow-700 container max-w-screen-2xl h-10 flex justify-between items-center bg-yellow-400">
             <div className="ml-5">
-                <Link to="/ProjekAkhir_NeatEase"><img src={logo} alt="NeatEase" w-4 /></Link>
+                <Link to="/TugasAkhir_NeatEase"><img src={logo} alt="NeatEase" w-4 /></Link>
             </div>
         </nav>
     );
